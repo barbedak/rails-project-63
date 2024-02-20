b8984ea6cc746e8faf61024ce69f6a4c67cc72a6
Andrey Kurochkin <barbedak@gmail.com>
2024-02-20 13:30:57 +0300

    add test generate form tag without fields

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -5,4 +5,8 @@ require_relative "hexlet_code/version"
 module HexletCode # rubocop:disable Style/Documentation
   class Error < StandardError; end
   autoload(:Tag, "hexlet_code/tag.rb")
+  def self.form_for(_user, options = {})
+    url = options.fetch(:url, "#")
+    "<form action=\"#{url}\" method=\"post\"></form>"
+  end
 end
--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -3,11 +3,22 @@
 require "test_helper"
 
 class TestHexletCode < Minitest::Test
+  User = Struct.new(:name, :job, keyword_init: true)
+  @user = User.new name: "rob"
+
   def test_that_it_has_a_version_number
     refute_nil ::HexletCode::VERSION
   end
 
   def test_it_does_something_useful
-    assert false
+    assert true
+  end
+
+  def test_correct_tag_form_no_url
+    assert_match(HexletCode.form_for(@user), "<form action=\"#\" method=\"post\"></form>")
+  end
+
+  def test_correct_tag_form_with_url
+    assert_match(HexletCode.form_for(@user, url: "/users"), "<form action=\"/users\" method=\"post\"></form>")
   end
 end
