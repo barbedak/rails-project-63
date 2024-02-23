f047bf2129199bc163e7f7fdb6ca4196488907db
Andrey Kurochkin <barbedak@gmail.com>
2024-02-23 11:57:45 +0300

    add tests check generation tag from block

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -5,8 +5,39 @@ require_relative "hexlet_code/version"
 module HexletCode # rubocop:disable Style/Documentation
   class Error < StandardError; end
   autoload(:Tag, "hexlet_code/tag.rb")
-  def self.form_for(_user, options = {})
+  attr_accessor :user
+
+  def self.form_for(user, options = {}, &block) # rubocop:disable Lint/UnusedMethodArgument
+    @user = user
     url = options.fetch(:url, "#")
-    "<form action=\"#{url}\" method=\"post\"></form>"
+    output = "<form action=\"#{url}\" method=\"post\">"
+    output << yield(*block_args) if block_given?
+    output.concat("</form>")
+  end
+
+  def self.input(field_name, options = {}) # rubocop:disable Metrics/MethodLength
+    as = options.fetch(:as, false)
+    if as
+      cols = options.fetch(:cols, false)
+      rows = options.fetch(:rows, false)
+      output = "<textarea name=\"#{field_name}\" "
+      output << if cols
+                  "cols=\"#{cols}\" "
+                else
+                  "cols=\"20\" "
+                end
+      output << if rows
+                  "rows=\"#{rows}\""
+                else
+                  "rows=\"20\""
+                end
+      output << ">#{@user.public_send(field_name.to_sym)}</textarea>"
+    else
+      output = "<input name=\"#{field_name}\" type=\"text\" value=\"#{@user.public_send(field_name.to_sym)}\""
+      klass = options.fetch(:class, false)
+      output << "class=\"#{klass}\"" if klass
+      output << ">"
+    end
+    output
   end
 end
--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -3,8 +3,8 @@
 require "test_helper"
 
 class TestHexletCode < Minitest::Test
-  User = Struct.new(:name, :job, keyword_init: true)
-  @user = User.new name: "rob"
+  User = Struct.new(:name, :job, :gender, keyword_init: true)
+  @user = User.new name: "rob", job: "hexlet", gender: "m"
 
   def test_that_it_has_a_version_number
     refute_nil ::HexletCode::VERSION
@@ -20,5 +20,15 @@ class TestHexletCode < Minitest::Test
 
   def test_correct_tag_form_with_url
     assert_match(HexletCode.form_for(@user, url: "/users"), "<form action=\"/users\" method=\"post\"></form>")
+  end
+
+  def test_correct_tag_for_with_block
+    user = User.new name: "rob", job: "hexlet", gender: "m"
+    result = HexletCode.form_for(user) do |f|
+      f.input :name
+      f.input :job, as: :text
+    end
+    expected = "<form action=\"#\" method=\"post\"><input name=\"name\" type=\"text\" value=\"rob\"><textarea name=\"job\" cols=\"20\" rows=\"40\">hexlet</textarea></form>" # rubocop:disable Layout/LineLength
+    assert_match(expected, result)
   end
 end
